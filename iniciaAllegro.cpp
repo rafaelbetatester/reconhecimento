c6c0835d28d3c8d7487a363497d82544bf2c8756
Rafael Rodrigues <rafaelbetatester@gmail.com>
2015-04-07 17:55:33 -0300

    Apagados arquivos inúteis

--- iniciaAllegro.cpp
+++
@@ -1,8 +0,0 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_native_dialog.h>
-
-void inicia_allegro()
-{
-	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
-}
--- iniciaAllegro.h
+++
@@ -1,6 +0,0 @@
-#ifndef INICIA_ALLEGRO
-#define INICIA_ALLEGRO
-
-void inicia_allegro();
-
-#endif
